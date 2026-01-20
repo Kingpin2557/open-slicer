e23134b524ab218edbd1dcb552e1d9a62da29d92
Kingpin2557 <lode.snaet@student.howest.be>
2026-01-20 20:27:13 +0100

    feat: new stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: resolve(__dirname, "src"),
   plugins: [react()],
-})
+  server: {
+    port: 8080,
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  },
+});
