43a6631ebb5218621b36cddae87bcbf518e9f805
Kingpin2557 <lode.snaet@student.howest.be>
2026-01-24 17:59:43 +0100

    fix: container flex to grid

--- src/components/IconField.tsx
+++ src/components/IconField.tsx
@@ -3,12 +3,12 @@ import Icon from "./Icon";
 type Icon = {
   type: string;
   size: number;
+  event: () => void;
 };
 
-function IconField({ type, size }: Icon) {
-  console.log(type);
+function IconField({ type, size, event }: Icon) {
   return (
-    <button className="c-icon" aria-label={type}>
+    <button className="c-icon" aria-label={type} onClick={event}>
       <Icon type={type} size={size} />
     </button>
   );
